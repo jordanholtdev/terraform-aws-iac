f846fcd809d875f07ba1c042d3fd89bea2478d19
jordanholtdev <github@jordanholt.dev>
2024-11-04 22:01:58 -0500

    add cloud-init script

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.aws_subnet_id
-
+  associate_public_ip_address = true 
+  user_data = file("../../scripts/add-users.yaml")
   tags = {
     Name = "App Server"
   }
